c8fc1ac3e2e5377ad9d0ab257b3797905f7cc45a
David Rönnqvist <david.ronnqvist@gmail.com>
2013-06-03 09:24:51 +0200

    Initial Commit

---
+++ CustomGeometry/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  CustomGeometry
+//
+//  Created by David Rönnqvist on 6/3/13.
+//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CustomGeometry/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CustomGeometry
+//
+//  Created by David Rönnqvist on 6/3/13.
+//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
